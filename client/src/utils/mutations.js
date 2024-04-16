3e8497f6cc09c2aa9f62885de44b14b7e0183fa8
DonnyAndre29 <donandre43@yahoo.com>
2024-04-15 18:11:58 -0700

    client/src/utils

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,48 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_POST = gql`
+  mutation addPost($userId: ID!, $post: String!) {
+    addPost(userId: $userId, post: $post) {
+      _id
+      username
+      posts   
+    }
+  }
+`;
+
+
+export const REMOVE_POST = gql`
+  mutation removePost($post: String!) {
+    removePost(post: $post) {
+      _id
+      name
+      posts
+    }
+  }
+`;
+
+
