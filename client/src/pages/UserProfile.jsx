23008f182e8e04747aae03bce5f5b19bcee1e0e1
DonnyAndre29 <donandre43@yahoo.com>
2024-04-16 19:53:25 -0700

    Client Folder

---
+++ client/src/pages/UserProfile.jsx
@@ -0,0 +1,64 @@
+import { Navigate, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+// import PostsList from '../components/PostsList';
+// import PostForm from '../components/PostForm';
+
+import { QUERY_SINGLE_USER, QUERY_YOU } from '../utils/queries';
+
+import Auth from '../utils/auth';
+
+const User = () => {
+  const { userId } = useParams();
+
+  // If there is no `profileId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
+  const { loading, data } = useQuery(
+    userId ? QUERY_SINGLE_USER : QUERY_YOU,
+    {
+      variables: { userId: userId },
+    }
+  );
+
+  // Check if data is returning from the `QUERY_YOU` query, then the `QUERY_SINGLE_USER` query
+  const user = data?.you || data?.user || {};
+
+  // Use React Router's `<Navigate />` component to redirect to personal profile page if username is yours
+  if (Auth.loggedIn() && Auth.getProfile().data._id === userId) {
+    return <Navigate to="/me" />;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see your profile page. Use the navigation
+        links above to sign up or log in!
+      </h4>
+    );
+  }
+
+  return (
+    <div>
+      <h2 className="card-header">
+        {userId ? `${user.username}'s` : 'Your'} friends have endorsed these
+        posts...
+      </h2>
+
+      {user.posts?.length > 0 && (
+        <PostsList
+          posts={user.posts}
+          isLoggedInUser={!userId && true}
+        />
+      )}
+
+      <div className="my-4 p-4" style={{ border: '1px dotted #1a1a1a' }}>
+        <PostForm userId={user._id} />
+      </div>
+    </div>
+  );
+};
+
+export default User;
