64684aaceac3440b66c2da2cb48f456a37547316
DonnyAndre29 <donandre43@yahoo.com>
2024-04-22 13:36:22 -0700

    vite package

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+// import reactRefresh from '@vitejs/plugin-react-refresh'
 // import css from 'vite-plugin-css'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   
-  plugins: [react(), reactRefresh()],
+  plugins: [react()],
   server: {
     port: 3001,
     open: true,
